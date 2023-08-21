d25abda7ae1ab62b6e0f5cddf4b08c89038f000a
Nitinpr1 <nitin.pr2002@gmail.com>
2023-08-21 18:49:38 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SuperQuality,
+  Subscribe,
+  CustomerReviews,
+  Footer,
+  SpecialOffer,
+};
